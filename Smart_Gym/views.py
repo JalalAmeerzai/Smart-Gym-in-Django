eaca7c46f4df3108c405b0d511b9da00eb4636fe
Jalal <Jalalx@msn.com>
2020-06-27 16:52:37 +0500

    Api testing code added

--- Smart_Gym/views.py
+++ Smart_Gym/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 def index(request):
     return render(request, 'index.html')
+
+@csrf_exempt
+def productapi(request):
+    if request.method=="POST":
+        #jalal = [{'id':'existing','name':'test user'}]
+        #jalal.append({'id': request.GET.get('id', ''), 'name': request.GET.get('name', '')})
+        #products = Product.objects.all()
+        #return HttpResponse(json.dumps(jalal,  default=str))
+        
+        
+        objectreturn = {}
+        for i in range(1,4):
+            obj = "obj"+str(i)
+            objectreturn[obj] = {"id": i, "name": "test"+str(i)}
+        objectreturn["obj"+str(request.POST.get('id', ''))] = {'id': request.POST.get('id', ''), 'name': json.loads(request.POST.get('name', ''))}
+
+        #converting string to json
+        y = json.loads(request.POST.get('name', ''))
+        print(y["obj1"])
+        return JsonResponse(objectreturn) 
