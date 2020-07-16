c0fb8cb8fecdbe2541cdabf18faa604e571a3384
Jalal <Jalalx@msn.com>
2020-07-16 05:05:09 +0500

    added partial admin templates

--- adminportal/urls.py
+++ adminportal/urls.py
@@ -3,7 +3,17 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name="adminportal-login"),
-    path('login/', views.login, name="adminportal-login")
+    path('login/', views.login, name="adminportal-login"),
+    path('passwordlost/', views.passwordlost, name="adminportal-passwordlost"),
+    path('dashboard/', views.dashboard, name="adminportal-dashboard"),
+    path('settings/', views.settings, name="adminportal-settings"),
+    path('user/', views.user, name="adminportal-user"),
+    path('staff/', views.staff, name="adminportal-staff"),
+    path('staffprofile/', views.staffprofile, name="adminportal-staffprofile"),
+    path('staffprofileedit/', views.staffprofileedit, name="adminportal-staffprofileedit"),
+    path('trainers/', views.trainers, name="adminportal-trainers"),
+    path('trainersprofile/', views.trainersprofile, name="adminportal-trainersprofile"),
+    path('trainersprofileedit/', views.trainersprofileedit, name="adminportal-trainersprofileedit"),
     #path('products/<int:myid>', views.products, name='shop-products'),
     #path('checkout/', views.checkout, name='shop-checkout'),
 ]
--- adminportal/views.py
+++ adminportal/views.py
@@ -1,5 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 def login(request):
+    if request.method == 'POST':
+        return redirect('/adminportal/dashboard/')
     return render(request, 'adminportal/login.html')
+
+
+
+def passwordlost(request):
+    return render(request, 'adminportal/lost-password.html')
+
+
+
+def dashboard(request):
+    return render(request, 'adminportal/dashboard.html')
+
+
+
+def settings(request):
+    return render(request, 'adminportal/settings.html')
+
+
+
+def user(request):
+    return render(request, 'adminportal/user.html')
+
+
+
+def staff(request):
+    return render(request, 'adminportal/staff.html')
+
+
+
+def staffprofile(request):
+    return render(request, 'adminportal/staffprofile.html')
+
+
+
+def staffprofileedit(request):
+    return render(request, 'adminportal/editstaff.html')
+
+
+
+def trainers(request):
+    return render(request, 'adminportal/trainers.html')
+
+
+
+def trainersprofile(request):
+    return render(request, 'adminportal/trainerprofile.html')
+
+
+def trainersprofileedit(request):
+    return render(request, 'adminportal/edittrainer.html')
