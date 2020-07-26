da063c1a9a06771c0d53f70b031fc8c051a6db24
Jalal <Jalalx@msn.com>
2020-07-26 14:26:35 +0500

    added classes logic in website

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from adminportal.models import ClassData, TrainerData
 
 def index(request):
     return render(request, 'website/index.html')
@@ -7,7 +8,36 @@ def index(request):
 
 
 def classes(request):
-    return render(request, 'website/classes.html')
+    params = {}
+    classes = ClassData.objects.all().order_by("class_stime")
+    classes_single ={}
+    timetable = {"monday": {}, "tuesday":{}, "wednesday":{}, "thursday":{}, "friday":{}, "saturday":{}, "sunday":{}}
+    for cl in classes:
+        try:
+            classes_single[cl.class_id] = {"name": cl.class_name, "desc": cl.class_desc, "img": cl.class_img_name, "trainer": TrainerData.objects.filter(trainer_id=cl.class_trainer)[0].trainer_name}
+            days = cl.class_days.split(" ")
+            if "Monday" in days:
+                timetable["monday"][cl.class_id] = {"name": cl.class_name, "trainer": TrainerData.objects.filter(trainer_id=cl.class_trainer)[0].trainer_name, "stime": cl.class_stime, "etime": cl.class_etime}
+            if "Tuesday" in days:
+                timetable["tuesday"][cl.class_id] = {"name": cl.class_name, "trainer": TrainerData.objects.filter(trainer_id=cl.class_trainer)[0].trainer_name, "stime": cl.class_stime, "etime": cl.class_etime}
+            if "Wednesday" in days:
+                timetable["wednesday"][cl.class_id] = {"name": cl.class_name, "trainer": TrainerData.objects.filter(trainer_id=cl.class_trainer)[0].trainer_name, "stime": cl.class_stime, "etime": cl.class_etime}
+            if "Thursday" in days:
+                timetable["thursday"][cl.class_id] = {"name": cl.class_name, "trainer": TrainerData.objects.filter(trainer_id=cl.class_trainer)[0].trainer_name, "stime": cl.class_stime, "etime": cl.class_etime}
+            if "Friday" in days:
+                timetable["friday"][cl.class_id] = {"name": cl.class_name, "trainer": TrainerData.objects.filter(trainer_id=cl.class_trainer)[0].trainer_name, "stime": cl.class_stime, "etime": cl.class_etime}
+            if "Saturday" in days:
+                timetable["saturday"][cl.class_id] = {"name": cl.class_name, "trainer": TrainerData.objects.filter(trainer_id=cl.class_trainer)[0].trainer_name, "stime": cl.class_stime, "etime": cl.class_etime}
+            if "Sunday" in days:
+                timetable["sunday"][cl.class_id] = {"name": cl.class_name, "trainer": TrainerData.objects.filter(trainer_id=cl.class_trainer)[0].trainer_name, "stime": cl.class_stime, "etime": cl.class_etime}
+        except Exception:
+            pass
+    
+    
+
+    params["classes"] = classes_single
+    params["timetable"] = timetable
+    return render(request, 'website/classes.html', params)
 
 
 
